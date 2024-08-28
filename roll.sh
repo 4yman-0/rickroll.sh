002e84c519f2e9bb715e0596237a68b221322aa5
4yman <170770027+4yman-0@users.noreply.github.com>
2024-08-28 11:26:50 +0100

    Update roll.sh

--- roll.sh
+++ roll.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
+#!/bin/env bash
 # Rick Astley in your Terminal.
 # By Serene and Justine Tunney <3
-version='1.2'
+version='1.2.1'
 rick='https://keroserene.net/lol'
 video="$rick/astley80.full.bz2"
-# TODO: I'll let someone with mac or windows machine send a pull request
-# to get gsm going again :)
+# TODO: Get gsm going again :)
 audio_gsm="$rick/roll.gsm"
 audio_raw="$rick/roll.s16"
 audpid=0
@@ -22,33 +21,33 @@ cleanup() { (( audpid > 1 )) && kill $audpid 2>/dev/null; }
 quit() { echo -e "\x1b[2J \x1b[0H ${purp}<3 \x1b[?25h \x1b[u \x1b[m"; }
 
 usage () {
-  echo -en "${green}Rick Astley performs ♪ Never Gonna Give You Up ♪ on STDOUT."
-  echo -e "  ${purp}[v$version]"
-  echo -e "${yell}Usage: ./astley.sh [OPTIONS...]"
-  echo -e "${purp}OPTIONS : ${yell}"
-  echo -e " help   - Show this message."
-  echo -e " inject - Append to ${purp}${USER}${yell}'s bashrc. (Recommended :D)"
+echo -en "\
+${green}Rick Astley performs ♪ Never Gonna Give You Up ♪ on STDOUT. $purp[v$version]
+${yell}Usage: ./astley.sh [OPTIONS...]
+${purp}OPTIONS : $yell
+  help   - Show this message.
+  inject - Append to $purp$USER$yell's bashrc. (Recommended :D)\
+"
 }
-for arg in "$@"; do
-  if [[ "$arg" == "help"* || "$arg" == "-h"* || "$arg" == "--h"* ]]; then
-    usage && exit
-  elif [[ "$arg" == "inject" ]]; then
-    echo -en "${red}[Inject] "
+
+case "$@" in
+  help|-h|--h)
+    usage
+    exit;;
+  inject)
+    echo -en "$red[Inject] "
     echo $NEVER_GONNA >> $MAKE_YOU_CRY
     echo -e "${green}Appended to $MAKE_YOU_CRY. <3"
-    echo -en "${yell}If you've astley overdosed, "
+    echo -en "${yell}If you've Astley overdosed, "
     echo -e "delete the line ${purp}\"$NEVER_GONNA\"${yell}."
-    exit
-  else
-    echo -e "${red}Unrecognized option: \"$arg\""
-    usage && exit
-  fi
-done
+    exit;;
+esac
+
 trap "cleanup" INT
 trap "quit" EXIT
 
 # Bean streamin' - agnostic to curl or wget availability.
-obtainium() {
+gimme() {
   if has? curl; then curl -s $1
   elif has? wget; then wget -q -O - $1
   else echo "Cannot has internets. :(" && exit
@@ -59,21 +58,21 @@ echo -en "\x1b[?25l \x1b[2J \x1b[H"  # Hide cursor, clear screen.
 #echo -e "${yell}Fetching audio..."
 if has? afplay; then
   # On Mac OS, if |afplay| available, pre-fetch compressed audio.
-  [ -f /tmp/roll.s16 ] || obtainium $audio_raw >/tmp/roll.s16
+  [ -f /tmp/roll.s16 ] || gimme $audio_raw >/tmp/roll.s16
   afplay /tmp/roll.s16 &
 elif has? aplay; then
   # On Linux, if |aplay| available, stream raw sound.
-  obtainium $audio_raw | aplay -Dplug:default -q -f S16_LE -r 8000 &
+  gimme $audio_raw | aplay -Dplug:default -q -f S16_LE -r 8000 &
 elif has? play; then
   # On Cygwin, if |play| is available (via sox), pre-fetch compressed audio.
-  obtainium $audio_gsm >/tmp/roll.gsm.wav
+  gimme $audio_gsm >/tmp/roll.gsm.wav
   play -q /tmp/roll.gsm.wav &
 fi
 audpid=$!
 
 #echo -e "${yell}Fetching video..."
-# Sync FPS to reality as best as possible. Mac's freebsd version of date cannot
-# has nanoseconds so inject python. :/
+# Sync FPS to reality as best as possible. Mac's freebsd version of date dosen't
+# have nanoseconds so inject python. :/
 python <(cat <<EOF
 import sys
 import time
@@ -95,4 +94,4 @@ try:
 except KeyboardInterrupt:
   pass
 EOF
-) < <(obtainium $video | bunzip2 -q 2> /dev/null)
+) < <(gimme $video | bunzip2 -q 2> /dev/null)
